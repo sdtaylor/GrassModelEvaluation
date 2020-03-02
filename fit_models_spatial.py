30b4975618c89376937ca9a8a33cdf9e0b48f335
Shawn Taylor <shawntaylor@weecology.org>
2020-03-02 16:46:08 -0700

    take out dask, put in load_model stuff

--- fit_models_spatial.py
+++ fit_models_spatial.py
@@ -3,26 +3,15 @@ import numpy as np
 import GrasslandModels
 
 from tools.load_data import get_pixel_modis_data
+from tools import load_models
 
-# from scipy import optimize
-# from time import sleep
-# import toolz
-
-# from dask_jobqueue import SLURMCluster
-# from dask.distributed import Client, as_completed
-# from dask import delayed
-# import dask
+from scipy import optimize
+from time import sleep
+import toolz
 
 from tools.tools import load_config
 config = load_config()
 
-# ceres_workers = 140 # the number of slurm job that will be spun up
-# ceres_cores_per_worker = 1 # number of cores per job
-# ceres_mem_per_worker   = '2GB' # memory for each job
-# ceres_worker_walltime  = '144:00:00' # the walltime for each worker, HH:MM:SS
-# ceres_partition        = 'medium'    # short: 48 hours, 55 nodes
-#                                     # medium: 7 days, 25 nodes
-#                                     # long:  21 days, 15 nodes
 
 
 
@@ -31,48 +20,32 @@ config = load_config()
 pixel_info = pd.read_csv('data/random_points.csv')
 pixel_info = pixel_info[pixel_info.percent_years_as_grass == 1]
 
-# subset to pixels just in WY & Eastern MT
+pixel_info = pixel_info[pixel_info.lat.between(41,49) & pixel_info.lon.between(-110,-104)]
+
+training_pixels = pixel_info[pixel_info.is_training].pixel_id.values
+
 wy_pixels = pixel_info[pixel_info.lat.between(41,49) & pixel_info.lon.between(-110,-104)].pixel_id.values
-
-#training_pixels = pixel_info[pixel_info.is_training].pixel_id.values
-
+training_pixels = [86]
 training_year_sets = [range(year_set['start'],year_set['end']+1) for year_set in config['training_year_sets']]
-######################################################
-# Setup dask cluster
-######################################################
-# cluster = SLURMCluster(processes=1,queue=ceres_partition, cores=ceres_cores_per_worker, memory=ceres_mem_per_worker, walltime=ceres_worker_walltime,
-#                        job_extra=[],
-#                        death_timeout=600, local_directory='/tmp/')
-
-# print('Starting up workers')
-# workers = cluster.scale(n=ceres_workers)
-# dask_client = Client(cluster)
-
-# active_workers =  len(dask_client.scheduler_info()['workers'])
-# while active_workers < (ceres_workers-1):
-#     print('waiting on workers: {a}/{b}'.format(a=active_workers, b=ceres_workers))
-#     sleep(5)
-#     active_workers =  len(dask_client.scheduler_info()['workers'])
-# print('all workers online')
-
-# # This is the callable sent to scipy.
-# def dask_scipy_mapper(func, iterable, c=dask_client):
-#     chunked_iterable = toolz.partition_all(10, iterable)
-#     results = [func(x) for x in chunked_iterable]
-#     futures =  dask_client.compute(results)
-#     return list(toolz.concat([f.result() for f in futures]))
 
 ######################################################
 # model fitting delayed(func)(x)
 ######################################################
+
+
+def mae_loss(obs, pred):
+    return np.nanmean(np.abs(obs-pred))
+
+loss_function = 'mean_cvmae'
+
 de_fitting_params = {
-                     'maxiter':500,
-                     'popsize':200,
-                     #'maxiter':20,
-                     #'popsize':4,
+                     'maxiter':1000,
+                     'popsize':50,
+                     #'maxiter':3,
+                     #'popsize':20,
                      'mutation':(0.5,1),
                      'recombination':0.25,
-                     'workers': 8,
+                     'workers': 1,
                      'polish': False,
 		     #'workers':2,
                      'disp':True}
@@ -81,17 +54,29 @@ de_fitting_params = {
 parameter_ranges = {'CholerPR1':{'b1':(0,200),
                                  'b2':(0,10),
                                  'b3':(0,10),
-                                 'L' :(0,6)},
+                                 'L' : 2},
                     'CholerPR2':{'b1':(0,200),
                                  'b2':(0,10),
                                  'b3':(0,10),
                                  'b4':(0,200),
-                                 'L' :(0,6)},
+                                 'L' : 2},
                     'CholerPR3':{'b1':(0,200),
                                  'b2':(0,10),
                                  'b3':(0,10),
                                  'b4':(0,200),
-                                 'L' :(0,6)},
+                                 'L' : 2},
+                    'CholerMPR2':{'b2':(0,10),
+                                  'b3':(0,10),
+                                  'b4':(0,200),
+                                  'L' :2},
+                    'CholerMPR3':{'b2':(0,10),
+                                  'b3':(0,10),
+                                  'b4':(0,200),
+                                  'L' :2},
+                    'CholerM1A':  {'a1':(0,10),
+                                  'a2':(0,10),
+                                  'a3':0,
+                                  'L': 2},
                     'PhenoGrassNDVI':{'b1': -1, # b1 is a not actually used in phenograss at the moment, 
                                                 # see https://github.com/sdtaylor/GrasslandModels/issues/2
                                                 # Setting to -1 makes it so the optimization doesn't waste time on b1
@@ -101,23 +86,33 @@ parameter_ranges = {'CholerPR1':{'b1':(0,200),
                                       'Phmax': (1,50),
                                       'Phmin': (1,50),
                                       'Topt': (0,45), 
-                                      'L': (0,6)},
+                                      'L': (0,8)},
                     'Naive' : {'b1':(0,100),
                                'b2':(0,100),
-                               'L': (0,6)}
+                               'L': (0,8)}
                     }
 
 
 if __name__=='__main__':
-
-    for training_years in training_year_sets:
-        ndvi, predictor_vars, _, _ = get_pixel_modis_data(years = training_years, pixels = wy_pixels)
-        for model_name in ['PhenoGrassNDVI','Naive']:
+    fit_models = []
+    
+    training_years = training_year_sets[0]
+    for training_years in training_year_sets[0:1]:
+        ndvi, predictor_vars, _, _ = get_pixel_modis_data(years = training_years, pixels = training_pixels)
+        
+        #for model_name in ['Naive','CholerPR1','CholerPR2','CholerPR3','PhenoGrassNDVI']:
+        for model_name in ['Naive','CholerMPR2','CholerMPR3','CholerPR1','CholerPR2','CholerPR3']:
+            
             local_model = GrasslandModels.utils.load_model(model_name)(parameters = parameter_ranges[model_name])            
             this_model_predictors = {p:predictor_vars[p] for p in local_model.required_predictors()}
             
-            local_model.fit(ndvi, this_model_predictors, optimizer_params = de_fitting_params, loss_function = 'mean_cvmae')
+            local_model.fit(ndvi, this_model_predictors, optimizer_params = de_fitting_params, loss_function = 'rmse')
             
-            model_filename = 'WY_{m}_{y1}{y2}.json'.format(m = model_name, y1=min(training_years), y2=max(training_years))
-            local_model.save_params('fitted_models/' + model_filename, overwrite=True)
+            fit_models.append(local_model)
     
+    
+    # compile all the models into a set and save
+    model_set = load_models.make_model_set(fit_models, note='fitting with only pixel_id==86, (in WY). still using custom smoothed (but not scaled) ndvi data.')
+    load_models.save_model_set(model_set)
+
+
